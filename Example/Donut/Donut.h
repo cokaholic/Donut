ecfa98d60b20497ed58c96f8fa520ae2ebb79063
cokaholic <nietzsche.god.is.dead@gmail.com>
2017-09-21 01:13:36 +0900

    support Carthage

---
+++ Example/Donut/Donut.h
@@ -0,0 +1,19 @@
+//
+//  Donut.h
+//  Donut
+//
+//  Created by 辰己 佳祐 on 2017/09/21.
+//  Copyright © 2017年 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Donut.
+FOUNDATION_EXPORT double DonutVersionNumber;
+
+//! Project version string for Donut.
+FOUNDATION_EXPORT const unsigned char DonutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Donut/PublicHeader.h>
+
+
